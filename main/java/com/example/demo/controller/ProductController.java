a8aefe38af1909953c431fe4dd69f71596b8eddf
ChristianManu <manuc@mymail.vcu.edu>
2020-10-26 14:02:27 -0400

    initial dev

---
+++ main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+
+@RestController
+public class ProductController {
+	
+	@Autowired
+	private ProductService service;
+	
+	@PostMapping("/addproduct")
+	public Product addProduct(Product product) {
+		return service.saveProduct(product);
+	}
+
+	@GetMapping("/product/{id}")
+	public Product findProductById(@PathVariable int id) {
+		return service.getProductById(id);
+	}
+	
+	
+	@GetMapping("/product/{name}")
+	public Product findProductByName(@PathVariable String name) {
+		return service.getProductByName(name);
+	}
+	
+	@PutMapping("/update")
+	public Product updateProduct(Product product) {
+		return service.updateProduct(product);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteProduct(int id) {
+		return service.deleteProduct(id); 
+	}
+}
+
