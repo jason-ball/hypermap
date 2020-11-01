aae96d7a5014b971fd5b16338438ada920096e64
ChristianManu <manuc@mymail.vcu.edu>
2020-11-01 10:31:36 -0500

    implemented lambock syntax, deketing getters and setters

---
+++ main/java/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	List<User> findAll();
+
+}
