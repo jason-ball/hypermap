1377437f16d72ad6b290f37e14e7c05258f1e90d
ChristianManu <manuc@mymail.vcu.edu>
2020-11-03 13:10:16 -0500

    new set up

---
+++ main/java/controller/SensorController.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import entity.Sensor;
+import exception.ResourceNotFoundException;
+import repository.SensorRepository;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+
+@RestController
+@RequestMapping("/api")
+public class SensorController {
+	@Autowired
+    private SensorRepository sensorRepository;
+	
+	@PostMapping("/Sensor")
+    public Sensor addEmployee(@RequestBody Sensor sensor) {
+        return sensorRepository.save(sensor);
+    }
+
+
+    @GetMapping("/Sensor")
+    public ResponseEntity<List<Sensor>> getAllSensors() {
+        return ResponseEntity.ok(sensorRepository.findAll());
+    }
+
+    
+    @GetMapping("Sensor/{SensorId}")
+    public ResponseEntity<Sensor> findEmployeeById(@PathVariable(value = "SensorId") Integer SensorId) {
+        Sensor sensor = sensorRepository.findById(SensorId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee not found" + SensorId));
+        return ResponseEntity.ok().body(sensor);
+    }
+    
+    @PutMapping("Sensor/{SensorId}")
+    public ResponseEntity<Sensor> updateSensor(@PathVariable(value = "SensorId") Integer SensorId,
+                                                   @RequestBody Sensor sensorDetails) {
+       Sensor sensor = sensorRepository.findById(SensorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Sensor not found for this id :: " + SensorId));
+        sensor.equals(sensor);
+        final Sensor updatedSensor = sensorRepository.save(sensor);
+        return ResponseEntity.ok(updatedSensor);
+
+    }
+    
+    @DeleteMapping("Sensor/{SensorId}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable(value = "SensorId") Integer SensorId) {
+        Sensor sensor = sensorRepository.findById(SensorId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee not found" + SensorId));
+        sensorRepository.delete(sensor);
+        return ResponseEntity.ok().build();
+    }
+
+}
--- main/java/entity/Sensor.java
+++ main/java/entity/Sensor.java
@@ -9,16 +9,42 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "Sensor")
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@Getter
+@Setter
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+
 
 public class Sensor {
-	@Id
-    @GeneratedValue
-    private int SensorId;
-    private String DisplayName;
+	
 
+    @Id
+    @GeneratedValue
+    private int SensoriId;
+    private String DisplayName;
+    
+    public Sensor() {}
+    
+    public Sensor(int SensorId, String DisplayName) {
+    	this.SensoriId = SensorId;
+    	this.DisplayName = DisplayName;
+    	
+    }
+
+	
 }
