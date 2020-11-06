b0dc0e1f8cb97772916cb6f7601e505ea5228e40
Jason Ball <jasonericball@gmail.com>
2020-11-06 14:05:08 -0500

    Add no-arg constructors for DB entities

--- server/src/main/java/hypermap/entity/HTTPLayer.java
+++ server/src/main/java/hypermap/entity/HTTPLayer.java
@@ -2,12 +2,14 @@ package hypermap.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "`HTTPLayer`")
