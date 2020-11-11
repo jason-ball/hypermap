43283d685282cf15a9da1bac769d2ef164d8c5c3
Jason Ball <jasonericball@gmail.com>
2020-11-10 23:42:26 -0500

    Add class for Admin UI Layer request

---
+++ server/src/main/java/hypermap/requestbody/AdminUILayerUploadRequest.java
@@ -0,0 +1,23 @@
+package hypermap.requestbody;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminUILayerUploadRequest {
+    @JsonProperty
+    private String displayName;
+
+    @JsonProperty
+    private String description;
+
+    @JsonProperty
+    private String file;
+
+    @JsonProperty
+    private String fileType;
+}
