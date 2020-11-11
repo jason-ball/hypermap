9b5472886645f443fd5ca6b635aed36d0cf43f93
Jason Ball <jasonericball@gmail.com>
2020-11-11 00:10:59 -0500

    Add PUT route for updating layers

--- server/src/main/java/hypermap/controller/SensorController.java
+++ server/src/main/java/hypermap/controller/SensorController.java
@@ -129,4 +129,36 @@ public class SensorController {
         geoJSONLayerRepository.save(layer);
     }
 
+    @PutMapping(path = "/MapLayer/GeoJSON")
+    public void updateGeoJson(@RequestBody AdminUILayerUploadRequest request) {
+        // Make a new layer object
+        GeoJSONLayer layer = geoJSONLayerRepository.getOne(request.getLayerID());
+
+        // Set the basic properties from the request
+        if (request.getDescription() != null) {
+            layer.setDescription(request.getDescription());
+        }
+        if (request.getDisplayName() != null) {
+            layer.setDisplayName(request.getDisplayName());
+        }
+
+        if (request.getFile() != null) {
+            // Get the GeoJSON from the request
+            String geoJSON = request.getFile();
+
+            // Remove the MIME type
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+            geoJSON = geoJSON.substring(geoJSON.indexOf(",") + 1);
+
+            // Decode the base64 encoded GeoJSON
+            geoJSON = new String(Base64.getDecoder().decode(geoJSON));
+
+            // Add the geoJSON to the layer
+            layer.setGeoJSON(geoJSON);
+        }
+
+        // Save it!
+        geoJSONLayerRepository.save(layer);
+    }
+
 }
