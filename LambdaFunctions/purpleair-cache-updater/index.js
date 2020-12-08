1a817f6424a68927901f305f134287a01f854587
Jason Ball <30378653+jason-ball@users.noreply.github.com>
2020-12-08 18:40:02 -0500

    Add air quality correction for PurpleAir cache lambda function

--- LambdaFunctions/purpleair-cache-updater/index.js
+++ LambdaFunctions/purpleair-cache-updater/index.js
@@ -12,11 +12,13 @@ exports.handler = async (event) => {
     const sensors = res.rows.flat().join('|');
     const purpleAirResponse = await axios.get(`https://www.purpleair.com/json?show=${sensors}`);
     
+    correctSensors(purpleAirResponse.data.results);
+    
     for (const sensor of purpleAirResponse.data.results) {
         try {
         await client.query({
-            text: 'INSERT INTO "PurpleAirHistory" ("PurpleAirID", "Time", "PM2.5", "Temperature", "Humidity") VALUES ($1, $2, $3, $4, $5)',
-            values: [sensor.ID, new Date(sensor.LastSeen * 1000), sensor.PM2_5Value, sensor.temp_f, sensor.humidity]
+            text: 'INSERT INTO "PurpleAirHistory" ("PurpleAirID", "Time", "PM2.5", "Temperature", "Humidity", "CorrectedPM2.5", "CorrectionMethod") VALUES ($1, $2, $3, $4, $5, $6, $7)',
+            values: [sensor.ID, new Date(sensor.LastSeen * 1000), sensor.PM2_5Value, sensor.temp_f, sensor.humidity, sensor.CorrectedPM2_5Value, sensor.CorrectionMethod]
         });
         } catch (err) {
             console.error(err.stack);
@@ -29,21 +31,19 @@ exports.handler = async (event) => {
     return "OK";
 };
 
-// function convertData(data) {
-//     let geoJSON = {
-//         type: 'FeatureCollection',
-//         features: []
-//     };
-//     for (const sensor of data.results) {
-//         geoJSON.features.push({
-//             type: 'Feature',
-//             id: `${sensor.ID}`,
-//             geometry: {
-//                 type: 'Point',
-//                 coordinates: [sensor.Lon, sensor.Lat]
-//             },
-//             properties: {...sensor}
-//         });
-//     }
-//     return geoJSON;
-// }
+function correctSensors(sensors) {
+    // PM2.5 = 0.38*PM2.5 + 2.94
+    // PM2.5 = 0.39*PM2.5 + 0.0024*T - 0.050*RH + 5.19
+    for (let sensor of sensors) {
+        if (sensor.PM2_5Value && sensor.temp_f && sensor.humidity) {
+            sensor.CorrectedPM2_5Value = (0.39 * parseFloat(sensor.PM2_5Value)) + (0.0024 * parseFloat(sensor.temp_f)) - (0.050 * parseFloat(sensor.humidity)) + 5.19;
+            sensor.CorrectionMethod = 'T & RH';
+        } else if (sensor.PM2_5Value) {
+            sensor.CorrectedPM2_5Value = (0.38 * parseFloat(sensor.PM2_5Value)) + 2.94;
+            sensor.CorrectionMethod = 'Linear';
+        } else {
+            sensor.CorrectedPM2_5Value = null;
+            sensor.CorrectionMethod = null;
+        }
+    }
+}
