69663a92466349928ae9a51ca52b9ce877b30cde
Jason Ball <30378653+jason-ball@users.noreply.github.com>
2020-12-08 18:38:51 -0500

    Add air quality correction to GeoJSON lambda function

--- LambdaFunctions/purpleair-geojson-proxy/index.js
+++ LambdaFunctions/purpleair-geojson-proxy/index.js
@@ -11,6 +11,7 @@ exports.handler = async (event) => {
     });
     const sensors = res.rows.flat().join('|');
     const purpleAirResponse = await axios.get(`https://www.purpleair.com/json?show=${sensors}`);
+    correctSensors(purpleAirResponse.data.results);
     const geoJSON = convertData(purpleAirResponse.data);
     const response = {
         statusCode: 200,
@@ -42,5 +43,108 @@ function convertData(data) {
             properties: {...sensor}
         });
     }
+    // BEGIN FAKE DATA
+        geoJSON.features.push({
+            type: 'Feature',
+            id: '99999',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.46117521084562, 37.56224603471652]
+            },
+            properties: {
+                Label: 'Fake Sensor: GOOD',
+                PM2_5Value: `${5}`,
+                CorrectedPM2_5Value: null,
+                CorrectionMethod: null
+            }
+        });
+        geoJSON.features.push({
+            type: 'Feature',
+            id: '99998',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.46117521084562, 37.56382683627568]
+            },
+            properties: {
+                Label: 'Fake Sensor: MODERATE',
+                PM2_5Value: `${20}`,
+                CorrectedPM2_5Value: null,
+                CorrectionMethod: null
+            }
+        });
+        geoJSON.features.push({
+            type: 'Feature',
+            id: '99997',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.46117521084562, 37.56532683627568]
+            },
+            properties: {
+                Label: 'Fake Sensor: UNHEALTHY SENSITIVE GROUPS',
+                PM2_5Value: `${40}`,
+                CorrectedPM2_5Value: null,
+                CorrectionMethod: null
+            }
+        });
+        geoJSON.features.push({
+            type: 'Feature',
+            id: '99996',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.46117521084562, 37.56682683627568]
+            },
+            properties: {
+                Label: 'Fake Sensor: UNHEALTHY',
+                PM2_5Value: `${100}`,
+                CorrectedPM2_5Value: null,
+                CorrectionMethod: null
+            }
+        });
+        geoJSON.features.push({
+            type: 'Feature',
+            id: '99995',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.46117521084562, 37.56832683627568]
+            },
+            properties: {
+                Label: 'Fake Sensor: VERY UNHEALTHY',
+                PM2_5Value: `${200}`,
+                CorrectedPM2_5Value: null,
+                CorrectionMethod: null
+            }
+        });
+        geoJSON.features.push({
+            type: 'Feature',
+            id: '99994',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.46117521084562, 37.56992683627568]
+            },
+            properties: {
+                Label: 'Fake Sensor: HAZARDOUS',
+                PM2_5Value: `${400}`,
+                CorrectedPM2_5Value: null,
+                CorrectionMethod: null
+            }
+        });
+        // END FAKE DATA
     return geoJSON;
+}
+
+function correctSensors(sensors) {
+    // PM2.5 = 0.38*PM2.5 + 2.94
+    // PM2.5 = 0.39*PM2.5 + 0.0024*T - 0.050*RH + 5.19
+    for (let sensor of sensors) {
+        if (sensor.PM2_5Value && sensor.temp_f && sensor.humidity) {
+            sensor.CorrectedPM2_5Value = (0.39 * parseFloat(sensor.PM2_5Value)) + (0.0024 * parseFloat(sensor.temp_f)) - (0.050 * parseFloat(sensor.humidity)) + 5.19;
+            sensor.CorrectionMethod = 'T & RH';
+        } else if (sensor.PM2_5Value) {
+            sensor.CorrectedPM2_5Value = (0.38 * parseFloat(sensor.PM2_5Value)) + 2.94;
+            sensor.CorrectionMethod = 'Linear';
+        } else {
+            sensor.CorrectedPM2_5Value = null;
+            sensor.CorrectionMethod = null;
+        }
+    }
 }
