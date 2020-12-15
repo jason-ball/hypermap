6cc99c48f50992bccfd9f9f44521cabc1ffbe8d1
Jason Ball <jasonericball@gmail.com>
2020-12-15 16:10:00 -0500

    Add Map Labels and Feature Clustering
    Signed-off-by: Jason Ball <jasonericball@gmail.com>

--- ui/src/app/map/map.component.ts
+++ ui/src/app/map/map.component.ts
@@ -3,7 +3,19 @@ import Map from "esri/Map";
 import MapView from "esri/views/MapView";
 import FeatureLayer from "esri/layers/FeatureLayer";
 import esriConfig from "esri/config";
-import GeoJSONLayer from "esri/layers/GeoJSONLayer";
+import GeoJSONLayer from 'esri/layers/GeoJSONLayer';
+import PopupTemplate from 'esri/PopupTemplate';
+import SimpleRenderer from 'esri/renderers/SimpleRenderer';
+import SimpleMarkerSymbol from 'esri/symbols/SimpleMarkerSymbol';
+import ColorVariable from 'esri/renderers/visualVariables/ColorVariable';
+import LabelClass from 'esri/layers/support/LabelClass';
+import FieldInfo from 'esri/popup/FieldInfo';
+import FieldsContent from 'esri/popup/content/FieldsContent';
+import FieldInfoFormat from 'esri/popup/support/FieldInfoFormat';
+import ExpressionInfo from 'esri/popup/ExpressionInfo';
+import FeatureReductionCluster from 'esri/layers/support/FeatureReductionCluster';
+import TextSymbol from 'esri/symbols/TextSymbol';
+import Font from 'esri/symbols/Font'
 
 @Component({
   selector: 'app-map',
@@ -21,7 +33,7 @@ export class MapComponent implements OnInit, OnDestroy {
   private _center: Array<number> = [0.1278, 51.5074];
   private _basemap = 'streets';
   private _loaded = false;
-  private _view: MapView = null; 
+  private _view: MapView = null;
 
   get mapLoaded(): boolean {
     return this._loaded;
@@ -59,44 +71,83 @@ export class MapComponent implements OnInit, OnDestroy {
   async initializeMap() {
 
     this.initializeWorkers();
-    
-    let sensorLabels:any[] = [];
-    const nameArcade = "$feature.PM2_5Value";
-    const nameClass = {
-      labelPlacement: "below-right",
+
+    const purpleAirLabels = new LabelClass({
+      labelPlacement: "center-center",
       minScale: 2500000,
       labelExpressionInfo: {
-        expression: nameArcade
+        expression: "Round($feature.CorrectedPM2_5Value, 2)"
       }
-    };
-    sensorLabels.push(nameClass)
+    });
 
+    const clusteredPurpleAirLabels = new LabelClass({
+      symbol: new TextSymbol({
+        color: 'white',
+        haloColor: "black",
+        haloSize: "1px",
+        font: new Font({
+          family: 'CalciteWebCoreIcons',
+          size: 12
+        })
+      }),
+      labelPlacement: 'center-center',
+      labelExpressionInfo: {
+        expression: '"\ue67a"' // esri-icon-zoom-in-magnifying-glass
+      }
+    })
 
-    const sensorRenderer = {
-      type: "simple", // autocasts as new SimpleRenderer()
-      symbol: {
-        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+    const sensorRenderer = new SimpleRenderer({
+      symbol: new SimpleMarkerSymbol({
         size: 30,
         color: [0, 244, 255, 1]
-      },
+      }),
       visualVariables: [
-        {
-          type: "color",
-          field: "PM2_5Value",
+        new ColorVariable({
+          field: "CorrectedPM2_5Value",
           stops: [
             { value: 0, color: "#00FF00" },
             { value: 50, color: "#FFFF00"},
             { value: 150, color: "#FF0000" }
           ]
-        }
-      ],
-      labelingInfo: [nameClass]
-    };
+        })
+      ]
+    });
 
-    const sensorDetailTemplate = {
-      title: "{Label}",
-      content: "PM 2.5: {PM2_5Value}"
-    };
+    const sensorPopupFields = new FieldsContent({
+      fieldInfos: [
+        new FieldInfo({
+          label: 'PM2.5 (Corrected)',
+          fieldName: 'CorrectedPM2_5Value'
+        }),
+        new FieldInfo({
+          label: 'PM2.5',
+          fieldName: 'pm2.5'
+        }),
+        new FieldInfo({
+          label: 'Correction Method',
+          fieldName: 'CorrectionMethod'
+        }),
+        new FieldInfo({
+          label: 'Last Updated',
+          fieldName: 'expression/sensorLastUpdated',
+          format: new FieldInfoFormat({
+            dateFormat: 'day-short-month-year-short-time'
+          }),
+        })
+      ]
+    })
+
+    const sensorDetailTemplate = new PopupTemplate({
+      title: "{name}",
+      content: [sensorPopupFields],
+      expressionInfos: [
+        new ExpressionInfo({
+          name: 'sensorLastUpdated',
+          title: 'Last Updated',
+          expression: '$feature.last_seen * 1000'
+        })
+      ]
+    });
 
     // const policeStopsLayer = new FeatureLayer({
     //   url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Denver_Police_Stops/FeatureServer/0",
@@ -104,9 +155,16 @@ export class MapComponent implements OnInit, OnDestroy {
     // });
 
     const purpleAirLayer = new GeoJSONLayer({
-      url: "https://k5emdaxun6.execute-api.us-east-1.amazonaws.com/dev/layer/42",
+      url: "https://k5emdaxun6.execute-api.us-east-1.amazonaws.com/dev/purpleair",
       popupTemplate: sensorDetailTemplate,
-      renderer: sensorRenderer as any
+      renderer: sensorRenderer,
+      labelingInfo: [purpleAirLabels],
+      featureReduction: new FeatureReductionCluster({
+        clusterRadius: 80,
+        clusterMinSize: 30,
+        clusterMaxSize: 30,
+        labelingInfo: [clusteredPurpleAirLabels]
+      })
     });
 
     // Configure the Map
@@ -115,7 +173,7 @@ export class MapComponent implements OnInit, OnDestroy {
       layers: [purpleAirLayer]
     };
 
-    const map = new Map(mapProperties);   
+    const map = new Map(mapProperties);
 
     // Initialize the MapView
     const mapViewProperties = {
@@ -125,11 +183,24 @@ export class MapComponent implements OnInit, OnDestroy {
       map: map
     };
 
-    
+
     this._view = new MapView(mapViewProperties);
-    
+
     // wait for the map to load
-    await this._view.when(); 
+    await this._view.when();
+
+    this._view.on('click', async (event) => {
+      const r = await this._view.hitTest(event.screenPoint);
+      r.results.forEach(result => {
+        if (result.graphic.attributes.clusterId) {
+          this._view.goTo({
+            target: result.graphic,
+            zoom: this._view.zoom + 2
+          })
+        }
+      })
+    })
+
     return this._view;
   }
 
@@ -174,11 +245,11 @@ export class MapComponent implements OnInit, OnDestroy {
         },
         { name: "tslib", location: `${DEFAULT_WORKER_URL}tslib`, main: "tslib" }
       ]
-    };    
+    };
   }
 
   ngOnInit() {
-    // For more info on change detection handling see: https://github.com/Esri/angular-cli-esri-map/issues/70
+    /* // For more info on change detection handling see: https://github.com/Esri/angular-cli-esri-map/issues/70
     // as well as this blog post: https://www.andygup.net/manual-change-detection-in-angular-for-performance/
     this.zone.runOutsideAngular(() => {
       // Initialize MapView and return an instance of MapView
@@ -188,7 +259,8 @@ export class MapComponent implements OnInit, OnDestroy {
         this._loaded = mapView.ready;
         this.mapLoadedEvent.emit(true);
       });
-    })
+    }) */
+    this.initializeMap();
   }
 
   ngOnDestroy() {
