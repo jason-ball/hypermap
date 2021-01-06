47d9bf2ca7ba385e89dd719a453eaa38800d9393
Jason Ball <jasonericball@gmail.com>
2021-01-06 00:00:44 -0500

    Add CORS config for production version
    Signed-off-by: Jason Ball <jasonericball@gmail.com>

--- server/src/main/java/hypermap/CorsConfig.java
+++ server/src/main/java/hypermap/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://localhost:4201", "https://hypermap.jasonball.dev", "https://admin.hypermap.jasonball.dev")
                 .allowedMethods("GET,POST,PUT,DELETE");
     }
 }
