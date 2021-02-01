d4a389f74be8007ee35eca25b311799d62f81e58
Jason Ball <jasonericball@gmail.com>
2021-02-01 12:40:27 -0500

    Add PurpleAirHistory GET route
    Signed-off-by: Jason Ball <jasonericball@gmail.com>

--- server/src/main/java/hypermap/controller/PurpleAirController.java
+++ server/src/main/java/hypermap/controller/PurpleAirController.java
@@ -1,2 +1,32 @@
-package hypermap.controller;public class PurpleAirController {
+package hypermap.controller;
+
+import hypermap.entity.PurpleAirHistory;
+import hypermap.repository.PurpleAirHistoryRepository;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/purpleair")
+@Api(tags = "PurpleAir")
+@CrossOrigin
+public class PurpleAirController {
+    private PurpleAirHistoryRepository purpleAirHistoryRepository;
+
+    @Autowired
+    public void setPurpleAirHistoryRepository(PurpleAirHistoryRepository purpleAirHistoryRepository) {
+        this.purpleAirHistoryRepository = purpleAirHistoryRepository;
+    }
+
+    @GetMapping("history")
+    public ResponseEntity<List<PurpleAirHistory>> getHistory() {
+        List<PurpleAirHistory> sensors = purpleAirHistoryRepository.findAll();
+        return ResponseEntity.ok(sensors);
+    }
 }
